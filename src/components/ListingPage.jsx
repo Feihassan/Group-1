f0123e2c45fb1c08fceb3ba3c7ef1e6a7a18ae87
kurui <kuruijoshua@gmail.com>
2025-07-23 09:55:40 +0300

    Fetch apartments from server and display titles

--- src/components/ListingPage.jsx
+++ src/components/ListingPage.jsx
@@ -76,5 +76,36 @@ function ListingPage() {
     </div>
   );
 }
+import React, { useEffect, useState } from "react";
+
+function ListingPage() {
+  const [apartments, setApartments] = useState([]);
+
+  useEffect(() => {
+    getApartments();
+  }, []);
+
+  function getApartments() {
+    fetch("http://localhost:3001/apartments")
+      .then((res) => res.json())
+      .then((data) => setApartments(data))
+      .catch((err) => console.log("Failed to fetch apartments:", err));
+  }
+
+  return (
+    <div>
+      <h2>All Apartments</h2>
+
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
+        {apartments.map((apt) => (
+          <div key={apt.id}>{apt.title}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+
 
 export default ListingPage;
