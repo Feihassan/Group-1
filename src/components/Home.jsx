8b30fd3e41aa5005ce30c57e56ad83709fc8187d
gladys <achandogladys6@gmail.com>
2025-07-23 10:13:34 +0300

    add new changes

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+function Home(){
+
+  return (
+    <div>
+      <h1>Welcom to Dream Apartment Finder</h1>
+      <p>
+        Discover your ideal apartment with ease.<br/>
+        Whether you,re relocating, upgrading your lifestyle, or simply exploring new neighborhoods-<br/>
+       <strong>Dream Apartment Finder</strong> helps you find a place that truly feels like home.<br/><br/>
+       Browse verified listings, filter by price, size, amenities or location<br/>
+       and save your favorites for later comparison.<br/>
+       You can even track updates in real time , so you'll never miss a new opportunity.<br/><br/>
+       Say goodbye to stressful apartment hunting.<br/>
+       Let us simplify your search and connect you to spaces where comfort meets convenience.
+      </p>
+
+    </div>
+
+  )
+}
+
+export default Home;
