94d9b7b9e349a97fdc49213cdf8b18373763cefd
gladys <achandogladys6@gmail.com>
2025-07-23 11:49:13 +0300

    add new changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router";
 
   return (
     <nav className="bg-white shadow-md p-4 flex items-center justify-between" >
-      <h1>Dream ApartMent Finder</h1>
-      <ul>
+      <h1 className="text-2xl font-bold text-blue-600">Dream ApartMent Finder</h1>
+      <ul className="flex space-x-6 text-gray-700 font-medium" >
         <li><Link to="/">Home</Link></li>
         <li><Link to="/listings">Listing</Link></li>
         <li><Link to="/ add">Add Apartment</Link></li>
