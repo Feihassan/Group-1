fe9f03eb78cb99cfd0a9493f7146777c5030966a
kurui <kuruijoshua@gmail.com>
2025-07-23 15:15:07 +0300

    new changes

--- src/components/ListingPage.jsx
+++ src/components/ListingPage.jsx
@@ -1,43 +1,14 @@
-import React, { useEffect, useState } from "react";
-import ApartmentCard from "./ApartmentCard";
+import React from "react";
 
 function ListingPage() {
-  const [apartments, setApartments] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/apartments")
-      .then((res) => res.json())
-      .then((data) => setApartments(data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  function handleBook(id) {
-    fetch(`http://localhost:3001/apartments/${id}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ booked: true })
-    })
-      .then((res) => res.json())
-      .then((updated) => {
-        const updatedList = apartments.map((apt) =>
-          apt.id === updated.id ? updated : apt
-        );
-        setApartments(updatedList);
-      });
-  }
-
   return (
-    <div className="max-w-7xl mx-auto p-6">
-      <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">
-        All Apartments
-      </h2>
-      <div className="flex flex-wrap justify-center gap-6">
-        {apartments.map((apt) => (
-          <ApartmentCards key={apt.id} apartment={apt} onBook={handleBook} />
-        ))}
+    <div>
+      <h2>All Apartments</h2>
+      <div>
+        {/* Apartments will go here */}
       </div>
     </div>
   );
-} 
+}
 
 export default ListingPage;
