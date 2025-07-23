3958735cde960d0705899407ce15e8d02e4ecc2c
kurui <kuruijoshua@gmail.com>
2025-07-23 11:59:13 +0300

    WIP: saving before rebase

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [apartments, setApartments] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/apartments')
+    fetch('http://localhost:3000/apartments')
       .then(res => res.json())
       .then(data => setApartments(data));
   }, []);
