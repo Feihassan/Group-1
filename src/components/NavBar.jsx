ed32b7e50a6a2a4b7bb5d96aab4740324db29dde
gladys <achandogladys6@gmail.com>
2025-07-23 11:52:27 +0300

    add new styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,9 +6,9 @@ import { Link } from "react-router";
     <nav className="bg-white shadow-md p-4 flex items-center justify-between" >
       <h1 className="text-2xl font-bold text-blue-600">Dream ApartMent Finder</h1>
       <ul className="flex space-x-6 text-gray-700 font-medium" >
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/listings">Listing</Link></li>
-        <li><Link to="/ add">Add Apartment</Link></li>
+        <li><Link to="/"className="hover:text-blue-500 transition" >Home</Link></li>
+        <li><Link to="/listings" className="hover:text-blue-500 transition" >Listing</Link></li>
+        <li><Link to="/ add" className="hover:text-blue-500 transition" >Add Apartment</Link></li>
       </ul>
     </nav>
   )
